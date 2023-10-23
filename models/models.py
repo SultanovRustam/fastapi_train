433feb04f127d9807a777d125dbb2db6cf71e5bd
Rustam <rust17@yandex.ru>
2023-10-23 11:26:12 +0300

    First commit

---
+++ models/models.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from sqlalchemy import Integer, String, TIMESTAMP, ForeignKey, Table, Column, JSON, Boolean
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Role(Base):
+    __tablename__ = "role"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    permissions = Column(JSON)
+
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    email = Column(String, nullable=False)
+    username = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    registered_at = Column(TIMESTAMP, default=datetime.utcnow)
+    role_id = Column(Integer, ForeignKey("role.id"), nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    is_superuser = Column(Boolean, default=False, nullable=False)
+    is_verified = Column(Boolean, default=False, nullable=False)
