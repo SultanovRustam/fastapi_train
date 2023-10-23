f1b54c5d32e43939978418e43e9a0580fcd71697
Rustam <rust17@yandex.ru>
2023-10-23 13:48:10 +0300

    rework structure

--- migrations/env.py
+++ migrations/env.py
@@ -4,8 +4,8 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
-from models.models import Role, User, Base
+from src.config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
+from src.auth.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- src/auth/manager.py
+++ src/auth/manager.py
@@ -1,16 +1,17 @@
 from typing import Optional
 
 from fastapi import Depends, Request
-from fastapi_users import BaseUserManager, IntegerIDMixin, models, schemas, exceptions
+from fastapi_users import BaseUserManager, IntegerIDMixin, exceptions, models, schemas
 
-from auth.database import User, get_user_db
+from auth.models import User
+from auth.utils import get_user_db
 
-SECRET = "SECRET"
+from config import SECRET_AUTH
 
 
 class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
-    reset_password_token_secret = SECRET
-    verification_token_secret = SECRET
+    reset_password_token_secret = SECRET_AUTH
+    verification_token_secret = SECRET_AUTH
 
     async def on_after_register(self, user: User, request: Optional[Request] = None):
         print(f"User {user.id} has registered.")
@@ -21,7 +22,6 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
         safe: bool = False,
         request: Optional[Request] = None,
     ) -> models.UP:
-
         await self.validate_password(user_create.password, user_create)
 
         existing_user = await self.user_db.get_by_email(user_create.email)
@@ -36,6 +36,7 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
         password = user_dict.pop("password")
         user_dict["hashed_password"] = self.password_helper.hash(password)
         user_dict["role_id"] = 1
+
         created_user = await self.user_db.create(user_dict)
 
         await self.on_after_register(created_user, request)
